493657ea7c34cc61bdbb432b1e3a3cdea5a3dc70
Gonzalo Sola <gfsola@afip.gob.ar>
2018-04-13 16:51:14 -0300

    archetype

--- generarProyecto.sh
+++ generarProyecto.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-clear
 
+crearDirectorio () {
+    mkdir $1
+}
+
+clear
 echo "-----------------------------------------------------------"
-echo "---	Generar projecto segun las propias              ---"
+echo "---   Generar projecto segun las propiedades elegidas   ---"
 echo "-----------------------------------------------------------"
 PS3="¿Que tipo de projecto quieres? "
 PS2="Monolitico:Busines,Domain,Standalone,Salt,VueJS Cloud:VueJS Salir"
@@ -15,40 +19,24 @@ do
         REPLY=
     else
         case $opcion in
-        1|M)
-        #"M:Monolitico:Busines,Domain,Standalone,VueJS"|"M")
-            break;;
-            #printf "Creando Monolitico"
+        "1"|"M"|"Monolitico:Busines,Domain,Standalone,Salt,VueJS")
+	    crearDirectorio()
+            mvn archetype:generate -DarchetypeCatalog=local
+            printf "Creando Monolitico"
+	    break;;
             #exit
-        2|C)
-        #"C:Cloud:VueJS"|"C")
-            #printf "Creando Cloud"
+        "2"|"C"|"Cloud:VueJS")
+            printf "Creando Cloud"
             break;;
-        3)
-        #"C:Cloud:VueJS"|"C")
-            echo "Aca"
+        "3"|"Salir")
+            echo "Saliendo..."
             exit
             break;;
         esac
     fi
 done
 
-   # if [ opcion ]; then
-      #  if ̣[[ -z $PS2 ]]; then
-       #     echo "algo"
-      #  else
-
-      #  printf "Opcion incorrecta\n"
-    #fi
-
-
-
-
-echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
-if [[ -n $1 ]]; then
-    echo "Last line of file specified as non-opt/last argument:"
-    tail -1 "$1"
-fi
+   
 #mkdir -p /opt/codigoFuenteMio
 #cd /opt/codigoFuenteMio
 #rm -rf $1

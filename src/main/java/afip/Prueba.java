d4160760b7e0e40629024ca2783f0cad66c380ce
Gonzalo Sola <gfsola@afip.gob.ar>
2018-04-13 13:20:48 -0300

    Invocacion Spring Boot

---
+++ src/main/java/afip/Prueba.java
@@ -0,0 +1,15 @@
+package afip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@ComponentScan
+//@EntityScan
+@SpringBootApplication
+public class Prueba {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Prueba.class, args);
+    }
+}
